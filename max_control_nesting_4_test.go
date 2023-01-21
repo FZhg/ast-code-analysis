bbd9c71b371f59904924f19231d3f58b923f327b
张凡 <zhangfanordomozhang@gmail.com>
2023-01-20 19:28:51 -0500

    modified filenames

--- max_control_nesting_4_test.go
+++ max_control_nesting_4_test.go
@@ -17,49 +17,49 @@ func Test_isMaxNestingMax4(t *testing.T) {
 		{
 			name: "for1",
 			args: args{
-				node: getRootNode("isMaxNestingMax4/for1.txt"),
+				node: getRootNode("max_control_nesting_4_testcases/for1.txt"),
 			},
 			want: false,
 		},
 		{
 			name: "for4",
 			args: args{
-				node: getRootNode("isMaxNestingMax4/for4.txt"),
+				node: getRootNode("max_control_nesting_4_testcases/for4.txt"),
 			},
 			want: false,
 		},
 		{
 			name: "for5",
 			args: args{
-				node: getRootNode("isMaxNestingMax4/for5.txt"),
+				node: getRootNode("max_control_nesting_4_testcases/for5.txt"),
 			},
 			want: true,
 		},
 		{
 			name: "if1",
 			args: args{
-				node: getRootNode("isMaxNestingMax4/if1.txt"),
+				node: getRootNode("max_control_nesting_4_testcases/if1.txt"),
 			},
 			want: false,
 		},
 		{
 			name: "if-(else-if)-if-else-if3",
 			args: args{
-				node: getRootNode("isMaxNestingMax4/if-(else-if)-if-else-if3.txt"),
+				node: getRootNode("max_control_nesting_4_testcases/if-(else-if)-if-else-if3.txt"),
 			},
 			want: false,
 		},
 		{
 			name: "if-(else-if)-if-else-if-else-if-if-if5",
 			args: args{
-				node: getRootNode("isMaxNestingMax4/if-(else-if)-if-else-if-else-if-if-if5.txt"),
+				node: getRootNode("max_control_nesting_4_testcases/if-(else-if)-if-else-if-else-if-if-if5.txt"),
 			},
 			want: true,
 		},
 		{
 			name: "switch-if-switch-if-for5.txt",
 			args: args{
-				node: getRootNode("isMaxNestingMax4/switch-if-switch-if-for5.txt"),
+				node: getRootNode("max_control_nesting_4_testcases/switch-if-switch-if-for5.txt"),
 			},
 			want: true,
 		},
@@ -67,7 +67,7 @@ func Test_isMaxNestingMax4(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := ExceedMaxNestingMax4(tt.args.node); got != tt.want {
-				t.Errorf("isMaxNestingMax4() = %v, want %v", got, tt.want)
+				t.Errorf("max_control_nesting_4_testcases() = %v, want %v", got, tt.want)
 			}
 		})
 	}
